586bbadc8ff8f52d926ac3d90ffa8497a68118af
C200543@ccg.ac.jp <C200543@ccg.ac.jp>
2021-11-25 09:34:07 +0900

    up

--- search.php
+++ search.php
@@ -14,11 +14,8 @@
             </div>
         </header>
     </div>
-
     <hr>
-
 <p>名前の一致するユーザーを検索し一覧で表示します</p>
-
     <div class="container">
         <form action="list.php" method="POST" class="row">
             <div class="col-sm-8 col-sm-offset-2">
@@ -26,16 +23,13 @@
                     <label for="name"><span >検索キーワード</span> </label>
                     <input type="text" id="name" name="name" class="form-control" value="" placeholder="例：山田" autofocus>
                 </div>
-
                 <div class="button-wrapper">
                     <button type="submit" class="btn btn--naby btn--shadow">検索する</button>
                 </div>
             </div>
         </form>
     </div>
-
     <hr>
-
     <div class="container">
         <footer>
             <p>CCC.</p>
